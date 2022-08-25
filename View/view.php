3cc829a6566dd5a09fe3c272b16e868d57ed014a
rovalentoni <rvalentoni@hotmail.com>
2022-08-25 18:54:25 -0300

    done

--- View/view.php
+++ View/view.php
@@ -14,13 +14,14 @@
     </tr>
     <?php 
         $showList = new Service;
-        $infoDb = $showList->selectSqlSave('SELECT * from test_table');
+        $info = $showList->selectSqlSave('SELECT * from test_table');
+        // print_r($info);
         // while ($infoDb = $showlist->serviceQuery('SELECT * FROM test_table') -> fetch_assoc()) {
         ?>
         <tr>
-            <td><?php echo $infoDb['id']; ?></td> <!-- Cria-se uma variável (infoDb), pega-se o resultado da query SELECT (selectSql) e aplica o método fetch_assoc, que guardará a informação da query na variável infoDb. -->
-            <td><?php echo $infoDb['ds_name']; ?></td>
-            <td><?php echo $infoDb['dt_birth']; ?></td>
+            <td><?php echo $info['id']; ?></td> <!-- Cria-se uma variável (infoDb), pega-se o resultado da query SELECT (selectSql) e aplica o método fetch_assoc, que guardará a informação da query na variável infoDb. -->
+            <td><?php echo $info['ds_name']; ?></td>
+            <td><?php echo $info['dt_birth']; ?></td>
         </tr>
     <?php //} ?>
 </table>
